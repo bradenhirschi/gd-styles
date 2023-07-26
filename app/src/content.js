ba57807cf06bdab954f99506a2d6d36cefb253aa
Braden Hirschi <bradenhirschi29@gmail.com>
2023-07-26 16:51:44 -0500

    Finished #4 locators

--- app/src/content.js
+++ app/src/content.js
@@ -1,5 +1,33 @@
+let targetElemt = null;
 const receiver = (message, sender, sendResponse) => {
-  console.log(message);
+  if (message.type === "getXPath") {
+    console.log(message);
+    parseDOM();
+  }
 };
 
 chrome.runtime.onMessage.addListener(receiver);
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  init();
+});
+
+const init = () => {
+  document.addEventListener(
+    "mousedown",
+    (event) => {
+      // This logs the element clicked on!!!
+      //    console.log(event.target);
+      // Save this ^
+      targetElemt = event.target;
+    },
+    false
+  );
+};
+
+const parseDOM = () => {
+  console.log(targetElemt);
+  console.log(targetElemt.id);
+  console.log(targetElemt.placeholder);
+  console.log(targetElemt.type);
+};
