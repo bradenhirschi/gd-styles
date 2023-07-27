b5f873c457cc5aa99d7753cce97bddda00d7676e
Braden Hirschi <bradenhirschi29@gmail.com>
2023-07-27 15:30:30 -0500

    Completed basic functionality with background-color and color

--- dist/content.js
+++ dist/content.js
@@ -1,7 +1,6 @@
 "use strict";
 let targetElement;
-// There's probably a good way to combine propertiesToDisplay and tailwindClassMap into the sama data structure
-const propertiesToDisplay = ["color", "background-color"];
+let propertiesToDisplay = ["color", "background-color"];
 // This receiver is added as a listener to accept the "getstyles" message from devtools.js
 const receiver = (message, sender, sendResponse) => {
     if (message.type === "getstyles") {
@@ -18,49 +17,55 @@ window.addEventListener("DOMContentLoaded", (event) => {
 });
 // This function gets the CSSStyleDeclaration from the targetElement, then gets the Tailwind styles from convertCssToTailwind, then passes the styles to the pane via message
 const parseElement = (element) => {
-    let style = getComputedStyle(element);
-    const targetElementProperties = [];
-    propertiesToDisplay.forEach((property) => {
-        const value = style.getPropertyValue(property);
-        if (value) {
-            targetElementProperties.push({ property: property, value: value });
-        }
-    });
-    const tailwindStyles = convertCssToTailwind(targetElementProperties);
-    let message = {
-        request: "sendtopane",
-        classList: tailwindStyles + style.backgroundColor,
-    };
-    chrome.runtime.sendMessage(message);
-};
-// Takes in an array of CSS property names and values and returns a Tailwind class list for the styles
-const convertCssToTailwind = (properties) => {
-    const tailwindClassListString = properties
-        .map(({ property, value }) => {
+    const styles = getComputedStyle(element);
+    const classList = propertiesToDisplay
+        .map((property) => {
         switch (property) {
             case "color":
-                return `text-[${rgbToHex(value)}]`;
+                if (styles.color !== "rgb(0, 0, 0)") {
+                    return `text-[${rgbaToHex(styles.color)}]`;
+                }
+                break;
             case "background-color":
+                if (styles.backgroundColor !== "rgba(0, 0, 0, 0)") {
+                    return `bg-[${rgbaToHex(styles.backgroundColor)}]`;
+                }
                 break;
         }
     })
         .join(" ");
-    return tailwindClassListString;
+    let message = {
+        request: "sendtopane",
+        classList: classList,
+    };
+    chrome.runtime.sendMessage(message);
 };
 // Convert rgb color to hexidecimal
-function rgbToHex(rgbColor) {
+function rgbaToHex(rgbaColor) {
     // Extract the individual color values from the string
-    const colorValues = rgbColor.match(/\d+/g);
+    const colorValues = rgbaColor.match(/(?:\d+(\.\d+)?)/g);
     // Ensure the color string is valid
-    if (!colorValues || colorValues.length !== 3) {
-        throw new Error('Invalid RGB color format. Expected format: "rgb(r, g, b)"');
+    if (!colorValues || (colorValues.length !== 3 && colorValues.length !== 4)) {
+        throw new Error('Invalid RGBA color format. Expected format: "rgba(r, g, b, a)"');
     }
     // Convert each color value to its hexadecimal representation
-    const hexValues = colorValues.map((value) => {
-        const intValue = parseInt(value, 10);
-        const hex = intValue.toString(16);
+    const hexValues = colorValues.slice(0, 3).map((value) => {
+        const intValue = parseFloat(value);
+        if (intValue < 0 || intValue > 255) {
+            throw new Error("RGB values must be in the range of 0 to 255.");
+        }
+        const hex = Math.round(intValue).toString(16);
         return hex.length === 1 ? `0${hex}` : hex;
     });
+    // Handle alpha value if present
+    let alphaHex = "";
+    if (colorValues.length === 4) {
+        const alphaValue = parseFloat(colorValues[3]);
+        if (alphaValue < 0 || alphaValue > 1) {
+            throw new Error("Alpha value must be in the range of 0 to 1.");
+        }
+        alphaHex = Math.round(alphaValue * 255).toString(16);
+    }
     // Combine the hexadecimal color values into the final string
-    return `#${hexValues.join("")}`;
+    return `#${hexValues.join("")}${alphaHex}`;
 }
