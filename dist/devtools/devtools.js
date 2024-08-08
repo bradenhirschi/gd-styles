fef62ac26ae179aa9f4f1edaec511621ba42798c
Braden Hirschi <braden@cloudpano.com>
2024-08-08 07:54:17 -0700

    Untracked dependencies and build files

--- dist/devtools/devtools.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-chrome.devtools.panels.elements.createSidebarPane("GD Styles", (pane) => {
-    chrome.devtools.panels.elements.onSelectionChanged.addListener(() => {
-        chrome.devtools.inspectedWindow.eval("parseElement($0)", {
-            useContentScriptContext: true,
-        });
-    });
-    pane.setPage("/src/pane/pane.html");
-});
